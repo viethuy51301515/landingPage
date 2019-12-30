6dfd28177203e906f91d24fd63c3130e0b4d9e01
Huy Phan <huy.phan@dou.com.vn>
2019-12-30 16:55:08 +0700

    'monday'

--- page/src/components/content/reason/reason.js
+++ page/src/components/content/reason/reason.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import './reason.scss'
-const bgImg = require('../../../assets/reason.jpg')
+import './reason.scss';
+import {Icon} from 'antd';
+const bgImg = require('../../../assets/reason.jpg');
+function ReasonItem(props){
+    return(
+        <div className='reason-item'>
+            <Icon type="plus-circle" style={{color:'white'}}/>
+            <h2>10 years of experience</h2>
+        </div>
+    )
+}
 class Reason extends React.Component{
     constructor(props){
         super(props);
@@ -8,12 +17,21 @@ class Reason extends React.Component{
     render(){
         return(
             <div className='reason-layout'>
-                <div className='reason-header' style={{backgroundImage:`url(${bgImg})`}}>
-                    <div className='reason-content'>
-                        <h1>
-                            <small>Vi sao </small>
-                            chon chung toi
-                        </h1>
+                <div className='reason-header'>
+                    <h1 className='title'>Why Choose Us</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing labore et dolore magna
+                    </p>
+                </div>
+                <div className='reason-content'>
+                    
+                    <div className='listItem' style={{backgroundImage:`url(${bgImg})`}}>
+                        {/* <div className='overlay'></div> */}
+                        <ReasonItem />
+                        <ReasonItem />
+                        <ReasonItem />
+                        <ReasonItem />
+                        <ReasonItem />
                     </div>
                 </div>
             </div>
